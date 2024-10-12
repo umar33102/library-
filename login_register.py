17b3f3f7e783216334324d32506ff893b84b84e8
umar33102 <160721413+umar33102@users.noreply.github.com>
2024-10-12 15:07:10 +0500

    Update login_register.py

--- login_register.py
+++ login_register.py
@@ -1,61 +1,6 @@
 
 
-# users = []
 
-# def register():
-#     print("Register: ")
-#     userName = input("Enter Your Name: ")
-#     password = input("Enter a password")
-#     if not userName or not password:
-#         print("Please write all field: ")
-#         return
-#     if userName in users:
-#         print("Already Exist User: ")
-#         return
-#     newUser = {"name" : userName, "password" : password}
-#     users.append(newUser)
-#     print(f"User '{userName} registered successfully: \n'")
-
-
-# def login():
-#     print("Login: ")
-#     userName = input("Enter Your Name: ")
-#     userFound = False
-#     for user in users:
-#         if user['name'] == userName:
-#             userFound = user
-#             break
-    
-#     if not userFound:
-#             print("Username not foun. Please register first.\n")
-#             return
-        
-#     password = input("Enter the password")
-#     if userFound['password'] == password:
-#         print(f"Welcome back, {userName}! \n")
-#     else:
-#         print("Incorrect password. Please try again. \n")
-
-
-# def main():
-#     while True:
-#         print("Welcome to the Login and Registration System")
-#         print("1. Register")
-#         print("2. Login")
-#         print("3. Exit")
-#         choice = int(input("Enter your choice (1/2/3)"))
-#         if choice == 1:
-#             register()
-#         elif choice == 2:
-#             login()
-#         elif choice == 3:
-#             print("Exiting the system. Goodbye!")
-#             break
-#         else:
-#             print("invalid choice. please try again. \n")
-
-# if __name__ == '__main__':
-#     main()
 
 
 users = []
